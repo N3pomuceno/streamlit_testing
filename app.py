8f4fd1b24c1a64baaef456ec6190c76e3b92007f
João Nepomuceno <jnfazevedo@hotmail.com>
2025-01-21 20:54:11 -0300

    prepara alterações para formulário

--- app.py
+++ app.py
@@ -10,17 +10,11 @@ st.set_page_config(layout="wide", page_title="Avaliação LLM", page_icon='🤖'
 # Nome do arquivo CSV onde os dados serão armazenados
 CSV_FILE_ANSWERS = "data/respostas_usuarios.csv"
 CSV_FILE_ORIGIN = "data/fr1.csv"
-FR_TEXT = "data/fr1.txt"
 
 # Verifica se o arquivo existe, se não, cria com colunas padrão
 if not os.path.exists(CSV_FILE_ANSWERS):
     pd.DataFrame(columns=["Texto 1", "Texto 2", "Resposta 1", "Resposta 2", "Resposta 3", "Comentários"]).to_csv(CSV_FILE_ANSWERS, index=True)
 
-# Verifica se o arquivo existe, se não, cria o texto padrão
-if not os.path.exists(FR_TEXT):
-    df = pd.read_csv(CSV_FILE_ORIGIN)
-    with open(FR_TEXT, "w") as f:
-        f.write("{}".format(df['fr'][0]))
 
 # Função para carregar os textos a partir de um CSV
 @st.cache_data
@@ -33,20 +27,26 @@ def select_random_texts(df):
     pair = random.sample(df.index.tolist(), 2)
     return df.loc[pair[0]], df.loc[pair[1]]
 
+
 # Título
 st.title("Avaliação LLM 🤖")
 
+df = pd.read_csv(CSV_FILE_ORIGIN)
+fr = df['fr'][0]
 # Instruções
-st.markdown("""
+st.markdown(f"""
 ### Bem-vindo!
-TEXTO INTRODUTÓRIO
-            
-Abaixo você encontrará dois textos gerados por um modelo de linguagem.
-            
-Vale a pena deixar acesso ao Documento que foi feita a análise?
-""")
 
-st.download_button('Download Documento Original', FR_TEXT)
+Abaixo você encontrará dois textos gerados por um modelo de linguagem.
+Leia os textos e responda as perguntas de avaliação.
+
+Veja o fato relevante abaixo:
+
+""")
+with st.expander("Fato Relevante"):
+    st.write(f'''
+        {fr}
+    ''')
 
 
 # Carrega os textos
@@ -72,10 +72,15 @@ st.markdown("### Avaliação")
 with st.form("avaliacao_form"):
     st.write("Responda as perguntas abaixo:")
     
-    resposta1 = st.radio("Qual texto é mais claro?", ["Texto 1", "Texto 2", "Ambos"])
-    resposta2 = st.radio("Qual texto é mais informativo?", ["Texto 1", "Texto 2", "Ambos"])
-    resposta3 = st.slider("Dê uma nota geral para os textos (1 a 10):", 1, 10)
-    comentarios = st.text_area("Comentários ou sugestões (opcional):")
+    resposta0 = st.radio("Alguma das análises apresentou informações incorretas ou inconsistentes em relação aos documentos financeiros analisados?", ["Nenhuma apresentou erro","A análise A apresentou erro", "A análise B apresentou erro", "Ambas apresentaram erros"])
+
+    resposta1 = st.radio("Qual análise é mais fácil de compreender, considerando estrutura, linguagem e clareza?", ["Análise 1", "Análise 2", "Ambas têm o mesmo nível de clareza."])
+
+    resposta2 = st.radio("Qual análise fornece as informações mais relevantes e aderentes ao fato relevante do documento?", ["Análise A", "Análise B", "Ambas são igualmente informativas"])
+
+    resposta3 = st.radio("De maneira geral, qual análise você considera mais útil para a tomada de decisões?", ["Análise A", "Análise B", "Ambas igualmente úteis"])
+
+    comentarios = st.text_area("Você gostaria de comentar algo sobre as análises apresentadas? (Opcional)")
     enviado = st.form_submit_button("Enviar")
 
     if enviado:
@@ -84,16 +89,18 @@ with st.form("avaliacao_form"):
         novo_dado = {
             "Texto 1": texto1["generated_text"],
             "Texto 2": texto2["generated_text"],
+            "Resposta 0": resposta0,
             "Resposta 1": resposta1,
             "Resposta 2": resposta2,
             "Resposta 3": resposta3,
             "Comentários": comentarios,
         }
 
-        novo_df = pd.DataFrame(novo_dado, index=[0])
+        # novo_df = pd.DataFrame(novo_dado, index=[0])
+
 
         try:
-            novo_df.to_csv(CSV_FILE_ANSWERS, mode="a", header=False, index=False)
+            # Salvar os dados
             st.success("Respostas enviadas com sucesso!")
         except Exception as e:
             st.error(f"Erro ao salvar os dados: {e}")

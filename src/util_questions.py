165e18fddb64beb689827ca017340ca6d2251d16
João Nepomuceno <jnfazevedo@hotmail.com>
2025-04-16 15:36:44 -0300

    Add: Adiciona arquivo faltante.

---
+++ src/util_questions.py
@@ -0,0 +1,36 @@
+RELEVANT_INFO = {
+    "texts": {
+        "Introduction": "",
+        "form_extent_intro": "### Responda as perguntas com o padrão de escala de 1 a 5, sendo 1 muito ruim e 5 muito bom.",
+        "new_fr_text": "",
+    },
+    "form_extent": {
+        "question0": {
+            "question": "##### **Fluência**: O texto é fácil de ler? Tem uma estrutura sintática boa e não contém erros linguísticos? Um texto com frases desconexas, erros de concordância ou vocabulário estranho é considerado não fluente.",
+            "options": ["1", "2", "3", "4", "5"],
+        },
+        "question1": {
+            "question": "##### **Coerência**: O conteúdo apresentado está logicamente organizado e faz sentido como um todo? Um parágrafo que começa falando de um tema e termina com algo completamente desconectado tem baixa coerência.",
+            "options": ["1", "2", "3", "4", "5"],
+        },
+        "question2": {
+            "question": "##### **Factualidade**: As informações fornecidas pelo texto são verdadeiras e baseadas em fatos verificáveis? Um texto que apresenta dados falsos ou distorcidos tem baixa factualidade.",
+            "options": ["1", "2", "3", "4", "5"],
+        },
+        "question3": {
+            "question": "##### **Aderência**: O conteúdo gerado está alinhado com o fato relevante fornecido? Um relatório que resume claramente os impactos financeiros de uma fusão entre empresas, conforme detalhado no fato relevante, demonstra alta aderência. Um relatório que discute o histórico da empresa ou tendências do setor, mas ignora os detalhes do fato relevante (como uma mudança na diretoria ou aquisição recente), apresenta baixa aderência ao tema.",
+            "options": ["1", "2", "3", "4", "5"],
+        },
+        "question4": {
+            "question": "##### **Utilidade**: Quanto o texto é útil, informativo e contribui de forma concreta para o entendimento ou a tomada de decisão? Um conteúdo com alta utilidade apresenta informações específicas, relevantes e com valor agregado. Um relatório que apresenta dados objetivos, análises detalhadas e implicações diretas para os acionistas demonstra alta utilidade. Um relatório que traz apenas opiniões vagas, frases genéricas ou observações evidentes (como “a empresa busca sempre crescer”) tem baixa utilidade.De maneira geral, qual análise você considera mais útil para a tomada de decisões?",
+            "options": ["1", "2", "3", "4", "5"],
+        },
+        "comentarios0": {
+            "question": "##### Você gostaria de comentar algo sobre a análise apresentada? (Opcional)",
+        },
+    },
+    "form_basic": {
+        "question0": {"Dados as análises acima, quais das duas você mais gostou?"},
+        "options": ["Análise A, Análise B"],
+    },
+}
